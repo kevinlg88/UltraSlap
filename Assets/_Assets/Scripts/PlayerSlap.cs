ea7977ab7ea390773d8bf0c9799cf45aeb8460a0
marisardo <marisardogd@gmail.com>
2025-03-14 23:35:39 -0300

    [UPDATE] Slap Cooldown
    -Update Slap with Slap Cooldown
    -New attack stance

--- Assets/_Assets/Scripts/PlayerSlap.cs
+++ Assets/_Assets/Scripts/PlayerSlap.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using MoreMountains.Feedbacks;
 using System.Diagnostics;
+using System.Runtime;
 
 public class PlayerSlap : MonoBehaviour
 {
@@ -16,10 +17,17 @@ public class PlayerSlap : MonoBehaviour
     [SerializeField] float chargingTime = 0f;
 
     [SerializeField] float minPower = 250;
-    [SerializeField] float maxPower = 1500;     //Variáveis para a definição da intensidade do tapa, de acordo com o tempo de carregamento
+    [SerializeField] float maxPower = 1500;
+    [SerializeField] float power;               //Variáveis para a definição da intensidade do tapa, de acordo com o tempo de carregamento
     [SerializeField] float growthRate = 1.5f;
     [SerializeField] float midPoint = 2.5f;
 
+    [SerializeField] float minCooldown = 0.1f;
+    [SerializeField] float maxCooldown = 0.8f;
+    [SerializeField] bool isOnCooldown = false;  //Definição e controle de slap cooldown
+    [SerializeField] float cooldownTimer = 0f;
+    [SerializeField] float cooldownGrowthRate = 0.001f;
+    [SerializeField] float cooldown;
 
     [SerializeField] private float quickSlapThreshold = 0.1f;
     [SerializeField] private float maxSlapThreshold = 5f;
@@ -36,20 +44,30 @@ public class PlayerSlap : MonoBehaviour
     {
 
 
-        if (Input.GetKeyDown(keySlap) && !isCharging && !isSlapping) 
+        if (Input.GetKeyDown(keySlap) && !isCharging && !isSlapping && !isOnCooldown) 
         {
             chargingTime = 0f;
             isCharging = true;
 
+            
             animator.SetTrigger("triggerTapa");
         }
 
 
         // Se o botão estiver pressionado, aumenta o tempo de carga
-        else if (Input.GetKey(keySlap) && isCharging)
+        else if (Input.GetKey(keySlap) && isCharging && !isOnCooldown)
         {
             chargingTime += Time.deltaTime;
 
+        }
+
+        else if (isOnCooldown)
+        {
+            cooldownTimer += Time.deltaTime;
+            if (cooldownTimer >= cooldown)
+            {
+                isOnCooldown = false;
+            }
         }
 
         else if (!Input.GetKeyDown(keySlap) && isSlapping && !animator.GetCurrentAnimatorStateInfo(0).IsTag("Slap"))
@@ -58,14 +76,16 @@ public class PlayerSlap : MonoBehaviour
         }
 
 
-        if (Input.GetKeyUp(keySlap))
+        else if (Input.GetKeyUp(keySlap) && !isOnCooldown)
         {
-            isCharging = false;
 
-            TriggerGetRagBone.SetSlapPower(minPower + (maxPower - minPower) / (1 + Mathf.Exp(-growthRate * (chargingTime - midPoint)))); // Define o valor do Slap Power para o Slap Hit Box
-            UnityEngine.Debug.Log(minPower + (maxPower - minPower) / (1 + Mathf.Exp(-growthRate * (chargingTime - midPoint))));
+            power = minPower + (maxPower - minPower) / (1 + Mathf.Exp(-growthRate * (chargingTime - midPoint))); // Calcula o valor de power
+            TriggerGetRagBone.SetSlapPower(power); // Define o valor do Slap Power para o Slap Hit Box
 
-            if (chargingTime <= quickSlapThreshold && !isSlapping)
+            //UnityEngine.Debug.Log("Slap:",power);
+
+
+            if (chargingTime <= quickSlapThreshold && !isSlapping) // Verifica se é um quick slap
             {
                
                 QuickSlap();
@@ -73,12 +93,11 @@ public class PlayerSlap : MonoBehaviour
             }
             else if (chargingTime > quickSlapThreshold && !isSlapping) // Caso contrário, é um charge slap
             {
+                
                 ChargedSlap();
             }
 
-            chargingTime = 0f;
         }
-
 
     }
 
@@ -93,29 +112,44 @@ public class PlayerSlap : MonoBehaviour
 
     void QuickSlap()
     {
-        animator.speed = 1f;
-        slapWhoosh.PlayFeedbacks();
-        isCharging = false;
-        isSlapping = true;
-        chargingTime = 0f;
+        if (!isSlapping) {
+
+            animator.speed = 1f;        // Retoma a animação ao soltar o botão  
+            chargingTime = 0f;
+            isSlapping = true;          // Bloqueia novos slaps até o cooldown
+            isCharging = false;
+        }
     }
 
     void ChargedSlap()
     {
-        if (animator.speed == 0f && !isCharging)
+
+        if (!isSlapping)
         {
-            // Retoma a animação ao soltar o botão  
+
             animator.speed = 1f;
-            slapWhoosh.PlayFeedbacks();
             isCharging = false;
-            isSlapping = true;
+            isSlapping = true; // Bloqueia novos slaps
+            isCharging = false;
         }
+    }
+
+    public void SlapFeedback()
+    {
+        slapWhoosh.PlayFeedbacks();
     }
 
     void SlappingEnd()
     {
+        cooldown = Mathf.Min(minCooldown + (power - minPower) * cooldownGrowthRate, maxCooldown);
+
+        animator.speed = 1f;
+
         chargingTime = 0f;
+        cooldownTimer = 0f;
         isSlapping = false;
         isCharging = false;
+        isOnCooldown = true;
+
     }
 }
